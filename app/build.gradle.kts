085735dbc983b59db6167b911aebdd685d60449a
Kelvin Andhika <kelvinandhika11@gmail.com>
2024-11-25 19:58:21 +0800

    Change Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "org.bangkit.kiddos_android"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.bangkit.kiddos_android"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
